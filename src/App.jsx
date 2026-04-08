58726a51456d3acee6d4d58aaa6eb7a4257eb364
DanicaLacandula <lacanduladanica@gmail.com>
2026-04-09 03:41:35 +0800

    Update App.jsx for app-shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import AuthCallback from './pages/AuthCallback'
 import Layout from './components/Layout'
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/callback" element={<AuthCallback />} />
         <Route path="/app" element={<Layout />} />
       </Routes>
     </BrowserRouter>
