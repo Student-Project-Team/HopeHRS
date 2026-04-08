7bf3176ebb245a8b51e1be713d33c9736bbebe62
DanicaLacandula <lacanduladanica@gmail.com>
2026-04-09 03:39:02 +0800

    Update App.jsx with register page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import AuthCallback from './pages/AuthCallback'
-import Layout from './components/Layout'
 
 export default function App() {
   return (
@@ -11,8 +9,6 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/callback" element={<AuthCallback />} />
-        <Route path="/app" element={<Layout />} />
       </Routes>
     </BrowserRouter>
   )
