add87c29e77b8a0f9025d0ba5290726a34d92ccf
Jherico Sabile <jhericosabile721@gmail.com>
2026-04-10 04:31:15 +0800

    Delete Desktop directory

--- Desktop/HopeHRS/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fexobarsdmxseexcpmta.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleG9iYXJzZG14c2VleGNwbXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNzMyMDYsImV4cCI6MjA5MDc0OTIwNn0.ig2fCOWt6xKKYbFDtEmXgXP0shs4YljomtWkIGs3PlA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
