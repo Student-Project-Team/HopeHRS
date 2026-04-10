36387c499da827ded7484c034dc39c0ed86a07b6
Jherico Sabile <jhericosabile721@gmail.com>
2026-04-10 15:42:19 +0800

    Delete feat-auth-google-oauth directory

--- feat-auth-google-oauth/src/pages/Employees.jsx
+++
@@ -1,9 +0,0 @@
-const Employees = () => {
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Employees Page</h1>
-            <p>Manage employee records here.</p>
-        </div>
-    )
-}
-export default Employees
