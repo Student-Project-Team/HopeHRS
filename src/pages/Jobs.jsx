93b48a6e8edcea866f1c05b11482fc1ac19eaaa6
Jherico Sabile <jhericosabile721@gmail.com>
2026-04-11 18:11:11 +0800

    Delete src directory

--- src/pages/Jobs.jsx
+++
@@ -1,9 +0,0 @@
-const Jobs = () => {
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Jobs Page</h1>
-            <p>Manage job listings here.</p>
-        </div>
-    )
-}
-export default Jobs
