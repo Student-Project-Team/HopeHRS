13cc9d2ab71aee4dab613bc248d8a77efcd4e5bc
Venice Marizene Linga <168013091+venicemarizene@users.noreply.github.com>
2026-04-12 20:54:35 +0800

    Fix formatting of AuthProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+      <AuthProvider>   
+        <App />
+      </AuthProvider>
   </StrictMode>
 )
