c7eacbe95a3e69b5d2e77201f4b5601d128d45ec
Danica Lacandula <lacanduladanica@gmail.com>
2026-05-01 01:09:20 +0800

    Fix useRights.js conflict - keep remote version

--- src/hooks/useRights.js
+++ src/hooks/useRights.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { useContext, useMemo } from 'react';
-import { UserRightsContext } from '../context/UserRightsContext';
-
-export function useRights() {
-  const { hasRight, rights } = useContext(UserRightsContext);
-  
-  const returnValue = useMemo(() => ({
-    rights,
-    canViewEmployees: () => hasRight('EMP_VIEW'),
-    canAddEmployee: () => hasRight('EMP_ADD'),
-    canEditEmployee: () => hasRight('EMP_EDIT'),
-    canDeleteEmployee: () => hasRight('EMP_DEL'),
-    
-    canViewJobHistory: () => hasRight('JH_VIEW'),
-    canAddJobHistory: () => hasRight('JH_ADD'),
-    canEditJobHistory: () => hasRight('JH_EDIT'),
-    canDeleteJobHistory: () => hasRight('JH_DEL'),
-    
-    canViewJobs: () => hasRight('JOB_VIEW'),
-    canAddJob: () => hasRight('JOB_ADD'),
-    canEditJob: () => hasRight('JOB_EDIT'),
-    canDeleteJob: () => hasRight('JOB_DEL'),
-    
-    canViewDepartments: () => hasRight('DEPT_VIEW'),
-    canAddDepartment: () => hasRight('DEPT_ADD'),
-    canEditDepartment: () => hasRight('DEPT_EDIT'),
-    canDeleteDepartment: () => hasRight('DEPT_DEL'),
-    
-    canManageUsers: () => hasRight('ADM_USER')
-  }), [hasRight, rights]);
-
-  return returnValue;
-=======
 import { useUserRights } from '../context/UserRightsContext';
 
 export function useRights() {
@@ -112,5 +78,4 @@ export function useRights() {
     canEditJobHistory,
     canDeleteJobHistory,
   };
->>>>>>> 6086850ae9d80e9457b07afb8278afa8644560f9
 }
