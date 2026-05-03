4fd98317844fbd38f9d052db704e440c2999688d
Jherico Sabile <jhericosabile721@gmail.com>
2026-05-03 08:38:11 +0800

    Delete src directory

--- src/utils/stamp.ts
+++
@@ -1,6 +0,0 @@
-
-export function makeStamp(action: string, userId: string): string {
-  const stamp = `${action} by ${userId} on ${new Date().toISOString()}`;
-  return stamp.substring(0, 60);
-}
- 
